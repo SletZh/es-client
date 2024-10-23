1f38454c6b469dc694849cd3a316c731d95742bb
张予坤 <zhangyukun@sinosoft.com.cn>
2024-10-23 22:02:13 +0800

    机构查询添加分页、排序、批量操作功能

--- src/main/java/com/zhangyk/esclient/repo/jpa/CompanyJpaRepository.java
+++ src/main/java/com/zhangyk/esclient/repo/jpa/CompanyJpaRepository.java
@@ -1,8 +1,8 @@
 package com.zhangyk.esclient.repo.jpa;
 
 import com.zhangyk.esclient.bean.Company;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompanyJpaRepository extends CrudRepository<Company,Long> {
+public interface CompanyJpaRepository extends JpaRepository<Company,Long> {
 
 }
